3711e327665b0ffa0775262133420c9c82136d0e
momin-riyadh <mominriyadh@gmail.com>
2024-09-23 11:53:09 +0600

    fix and deplo from cursor suggestion

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const listEl = document.querySelector("#items");
 const cartEl = document.querySelector(".cartItems");
-const carTitleEl = document.querySelector(".cartTitle");
+const cartTitleEl = document.querySelector(".cartTitle");
 const cartTotalEl = document.querySelector("#cartTotal");
 const cartBtmContinerEl = document.querySelector(".btmContainer");
 const confOverlay = document.querySelector(".confContainer");
@@ -15,60 +15,64 @@ let cartQty = 0;
 let cartTotal = 0;
 
 async function getProducts() {
-  const res = await fetch(requestURL);
-  let data = await res.json();
-  products = data;
+  try {
+    const res = await fetch(requestURL);
+    let data = await res.json();
+    products = data;
 
-  confOverlay.style.display = "none";
+    confOverlay.style.display = "none";
 
-  products.forEach((el) => {
-    listEl.insertAdjacentHTML(
-      "beforeend",
-      `
-      <div class="itemCard block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-        <div class="itemImg">
-            <img src="${el.image}" class="h-auto max-w-full" alt>
-          <button type="button" class="cartAddBtn" class="productImg" id="btn-${
-            el.id
-          }" onclick="addToCart(${el.id})">
-                  <img src="/assets/images/icon-add-to-cart.svg" id="addCartBtn">
-              <p>Add to Cart</p>
-          </button>
-          <button type="button" class="cartAddedBtn" id="btnInCart-${el.id}">
-              <div class="chgQtyBtn" id="itmDecBtn-${
-                el.id
-              }" onclick = "decCart(${el.id})">
-               <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-dash-circle" viewBox="0 0 16 16">
-                  <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
-                  <path d="M4 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 4 8"/>
-                </svg>
-              </div>
-              <p id="btnQty-${el.id}">QTY</p>
-              <div class="chgQtyBtn" id="itmIncBtn-${
-                el.id
-              }" onclick = "addToCart(${el.id})">
-               <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-circle" viewBox="0 0 16 16">
-                  <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
-                  <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4"/>
-                </svg>
-              </div>
-          </button>
-        </div>
-        <div class="itemInfo">
-          <p class="itemInfoCategory">${el.category}</p>
-          <p class="itemInfoName">${el.title}</p>
-          <p class="itemInfoPrice">$${el.price.toFixed(2)}</p>
-        </div>
-      </div>`
-    );
-  });
+    products.forEach((el) => {
+      listEl.insertAdjacentHTML(
+        "beforeend",
+        `
+        <div class="itemCard block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+          <div class="itemImg">
+              <img src="${el.image}" class="h-auto max-w-full" alt>
+            <button type="button" class="cartAddBtn" class="productImg" id="btn-${
+              el.id
+            }" onclick="addToCart(${el.id})">
+                    <img src="/assets/images/icon-add-to-cart.svg" id="addCartBtn">
+                <p>Add to Cart</p>
+            </button>
+            <button type="button" class="cartAddedBtn" id="btnInCart-${el.id}">
+                <div class="chgQtyBtn" id="itmDecBtn-${
+                  el.id
+                }" onclick = "decCart(${el.id})">
+                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-dash-circle" viewBox="0 0 16 16">
+                    <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
+                    <path d="M4 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 4 8"/>
+                  </svg>
+                </div>
+                <p id="btnQty-${el.id}">QTY</p>
+                <div class="chgQtyBtn" id="itmIncBtn-${
+                  el.id
+                }" onclick = "addToCart(${el.id})">
+                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-circle" viewBox="0 0 16 16">
+                    <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
+                    <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4"/>
+                  </svg>
+                </div>
+            </button>
+          </div>
+          <div class="itemInfo">
+            <p class="itemInfoCategory">${el.category}</p>
+            <p class="itemInfoName">${el.title}</p>
+            <p class="itemInfoPrice">$${el.price.toFixed(2)}</p>
+          </div>
+        </div>`
+      );
+    });
+  } catch (error) {
+    console.error('Error fetching products:', error);
+  }
 }
 
 function updateCart(clickedID) {
   if (cart.length === 0) {
     cartBtmContinerEl.style.visibility = "hidden";
     cartBtmContinerEl.style.height = "0";
-    carTitleEl.innerHTML = `Your Cart (${cartQty})`;
+    cartTitleEl.innerHTML = `Your Cart (${cartQty})`;
     cartEl.style.alignItems = "center";
     cartEl.style.justifyContent = "space-evenly";
 
@@ -84,25 +88,17 @@ function updateCart(clickedID) {
           <div class="cartItem">
             <span>
               <p id="cartItmName">${el.itemName}</p>
-              <p class="cartItmPricing" id="cartItmQty">${
-                el.itemQty
-              }     x     </p>
-              <p class="cartItmPricing" id="cartItmUnitPrice"> $${
-                el.itemPrice
-              }</p>
-              <p class="cartItmPricing" id="cartItmUnitTotal">$${(
-                el.itemQty * el.itemPrice
-              ).toFixed(2)}</p>
+              <p class="cartItmPricing" id="cartItmQty">${el.itemQty} x </p>
+              <p class="cartItmPricing" id="cartItmUnitPrice">$${el.itemPrice}</p>
+              <p class="cartItmPricing" id="cartItmUnitTotal">$${(el.itemQty * el.itemPrice).toFixed(2)}</p>
             </span>
-            <button type="button" id="btn-remove-${el.id}" onclick="rmvCart(${
-        el.id
-      })">
+            <button type="button" id="btn-remove-${el.id}" onclick="rmvCart(${el.id})">
                 <svg xmlns="http://www.w3.org/2000/svg" width="10" height="10" fill="none" viewBox="0 0 10 10"><path fill="#CAAFA7" d="M8.375 9.375 5 6 1.625 9.375l-1-1L4 5 .625 1.625l1-1L5 4 8.375.625l1 1L6 5l3.375 3.375-1 1Z"/></svg>
             </button>
         </div>`;
     });
 
-    carTitleEl.innerHTML = `
+    cartTitleEl.innerHTML = `
       Your Cart (${cartQty})
     `;
     cartTotalEl.innerHTML = `
@@ -214,6 +210,7 @@ function rmvCart(clickedID) {
 }
 
 function confirmCart() {
+  confOverlayCart.innerHTML = ""; // Clear previous content
   cart.forEach((el) => {
     confOverlayCart.innerHTML += `
       <div class="confCartContainer">
@@ -221,15 +218,11 @@ function confirmCart() {
           <img src="${el.itemImg}" class="w-10 h-10 rounded-full" alt="">
           <span>
             <p id="cartItmName">${el.itemName}</p>
-            <p class="cartItmPricing" id="cartItmQty">${el.itemQty}x     </p>
-            <p class="cartItmPricing" id="cartItmUnitPrice">@ $${
-              el.itemPrice
-            }</p>
+            <p class="cartItmPricing" id="cartItmQty">${el.itemQty}x</p>
+            <p class="cartItmPricing" id="cartItmUnitPrice">@ $${el.itemPrice}</p>
           </span>
           <div class="cartItmtTotalCnt">
-            <p class="cartItmPricing" id="confCartItmUnitTotal">$${(
-              el.itemQty * el.itemPrice
-            ).toFixed(2)}</p>
+            <p class="cartItmPricing" id="confCartItmUnitTotal">$${(el.itemQty * el.itemPrice).toFixed(2)}</p>
           </div>
         </div>
       </div>`;
@@ -263,6 +256,6 @@ function newOrder() {
   confOverlayBgBlur.style.visibility = "hidden";
 }
 
-confirmCart();
-getProducts();
-newOrder();
+document.addEventListener('DOMContentLoaded', () => {
+  getProducts();
+});
